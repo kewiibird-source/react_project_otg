18652a040601d3309e34e89aef9b8a1376c2705d
kewiibird <kewiibird@gmail.com>
2026-06-09 14:52:11 +0900

    Fix : console 제거

--- express-back/app.js
+++ express-back/app.js
@@ -12,10 +12,10 @@ const notificationRouter = require("./routes/notifications");
 const db = require("./db");
 
 const app = express();
-app.use((req, res, next) => {
-    console.log(`[LOG] ${req.method} ${req.url}`);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log(`[LOG] ${req.method} ${req.url}`);
+//     next();
+// });
 app.use(cors());
 app.use(express.json())
 
